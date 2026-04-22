e25d6315bd7f4d63b842435416c5873bfa787433
liong999 <nagara987@gmail.com>
2026-04-22 17:17:51 +0700

    ADD : More description

--- tw_map_widget/__manifest__.py
+++ tw_map_widget/__manifest__.py
@@ -13,6 +13,7 @@
     'category': 'Tools/UI',
     'version': '18.0.1.0.0',
     'license': 'LGPL-3',
+    'images': ['static/description/banner.png'],
     'depends': ['base', 'web', 'base_geolocalize'],
     'data': [
         # views, security, etc.
